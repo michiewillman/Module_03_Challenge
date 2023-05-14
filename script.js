4ed73f39d3922c9ccff95aef5c550e3abe3392ff
Michie <contact@michiewillman.com>
2023-05-13 17:08:25 -0700

    Added notes to source code.

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // Assignment Code
+
+//  Note: querySelector() returns the first Element within the document that matches the specified selector, or group of selectors
+//  Reminder: a Selector is used in HTML/CSS to style and reference elements
+//  Breakdown: This variable line is establishing the first element in the document object that qualifies as having the selector "#generate", which is the button in index.html that has the id="generate" aka #generate
+
 var generateBtn = document.querySelector("#generate");
+
+//...prompts user for password...
+
+// When prompted for password criteria
+//  I select which criteria to include in the password
+// When prompted for the length of the password
+//  I choose a length of at least 8 characters and no more than 128 characters
+// When asked for character types to include in the password
+//  I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+// I answer each prompt
+//  my input should be validated and at least one character type should be selected
+// All prompts are answered
+//  a password is generated that matches the selected criteria
+// Password is generated
+//  the password is either displayed in an alert or written to the page
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +34,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+

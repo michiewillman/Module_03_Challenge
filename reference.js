24cba7857e57684780f49495b7b8347fa9ca2fb6
Michie <contact@michiewillman.com>
2023-05-17 22:12:15 -0700

    Add alert if user does not select chars

--- reference.js
+++ reference.js
@@ -10,3 +10,20 @@ function sum(...numbers) {
 }
 
 console.log(sum(1, 2, 3, 4, 5, 6, 100));
+
+
+
+    // Declare functions to generate random characters if needed
+    function randomNumber() {
+      Math.floor(Math.random() * 10);
+    } 
+    function randomLower() {
+      alpha[Math.floor(Math.random() * alpha.length)];
+    }
+    function randomUpper() {
+      alpha[Math.floor(Math.random() * alpha.length)].toUpperCase();
+    }
+    function randomSpecial() {
+      special[Math.floor(Math.random() * special.length)];
+    }
+  
--- script.js
+++ script.js
@@ -5,64 +5,43 @@
 //  Breakdown: This variable line is establishing the first element in the document object that qualifies as having the selector "#generate", which is the button in index.html that has the id="generate" aka #generate
 var generateBtn = document.querySelector("#generate");
 
-// When prompted for password criteria
-//  I select which criteria to include in the password
-// When prompted for the length of the password
-//  I choose a length of at least 8 characters and no more than 128 characters
-// When asked for character types to include in the password
-//  I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// I answer each prompt
-//  my input should be validated and at least one character type should be selected
-// All prompts are answered
-//  a password is generated that matches the selected criteria
-// Password is generated
-//  the password is either displayed in an alert or written to the page
+// Set all character choice values
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 
-  // Set all character choice values
-  var numbers = [0,1,2,3,4,5,6,7,8,9];
-  var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-  var passLength = getPassLength();
-
-  // Get password length choice from user
-  function  getPassLength() {
-    var passLength = parseInt(prompt("How many characters should the password be?", "Must be between 8 and 128."));
-
-    // Repeat function if length does not meet requirements
-    if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-      alert("Sorry, your entry must be a number between 8 and 128.");
-      getPassLength();
-    } else {
-      return passLength;
-    }
-  }
 
   // Generate password using user's character choices
   function generatePassword() {
+    
+    // Get password length choice from user
+    var passLength = getPassLength();
+    function  getPassLength() {
+      var passLength = parseInt(prompt("How many characters should the password be?", "Must be between 8 and 128."));
+
+      // Repeat function if length does not meet requirements
+      if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+        alert("Sorry, your entry must be a number between 8 and 128.");
+        getPassLength();
+      } else {
+        return passLength;
+      }
+    }
+
     var useNumbers = confirm("Should the password use numbers?");
     var useLower = confirm("Should the password use Lowercase letters?");
     var useUpper = confirm("Should the password use Uppercase letters?");
     var useSpecial = confirm("Should the password use special characters?");
 
+    if (!useNumbers && !useLower && !useUpper && !useSpecial) {
+      alert("You must choose at least one character type to use in the password.");
+    } else {
+
     var choiceString = "";
     var newPassword = "";
 
-    // Declare functions to generate random characters if needed
-    // function randomNumber() {
-    //   Math.floor(Math.random() * 10);
-    // } 
-    // function randomLower() {
-    //   alpha[Math.floor(Math.random() * alpha.length)];
-    // }
-    // function randomUpper() {
-    //   alpha[Math.floor(Math.random() * alpha.length)].toUpperCase();
-    // }
-    // function randomSpecial() {
-    //   special[Math.floor(Math.random() * special.length)];
-    // }
-  
     // Based on user inputs, these variables will be true or false
     if (useUpper) {
       choiceString += upperLetters.join("");
@@ -83,9 +62,9 @@ var generateBtn = document.querySelector("#generate");
     }
   
     return newPassword;
+    
+    }
   }
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -103,4 +82,4 @@ generateBtn.addEventListener("click", writePassword);
 writePassword();
 
 // // //Alerts the user of the new password
-window.alert("Your new password has been generated.");
+alert("A new password has been generated.");
